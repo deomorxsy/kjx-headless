596f08399ddf84bb25b66526027f5be53b361edb
asari <32879431+deomorxsy@users.noreply.github.com>
2024-11-27 09:37:43 -0300

    add: prometheus exporter script, reuse ebpf_exporter and export to local grafana deployed on k3s

---
+++ scripts/prom-scraper.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# get local dynamic ip
+DYNIP=$(ip route get 8.8.8.8 | awk 'NR==1 {print $7}')
+
+cat  > ./deploy/monitoring/prom-metrics_ebpfexp.yaml <<EOF
+global:
+  scrape_interval: 15s
+  evaluation_interval: 15s
+
+scrape_configs:
+  - job_name: 'ebpf_exporter'
+    static_configs:
+      - targets: ['$DYNIP:9435']
+
+  - job_name: 'prometheus'
+    scrape_interval: 1m
+    static_configs:
+      - targets: ['localhost:9100']
+
+remote_write:
+  - url: '<Your Prometheus remote_write endpoint>'
+    basic_auth:
+      username: '<Your Grafana Username>'
+      password: '<Your Grafana Cloud Access Policy Token>'
+
+
+EOF
+
+# prom/prometheus:latest
+podman run \
+    --rm -it \
+    --name prometheus \
+    -p 9090:9090 \
+    --net=host \
+    -v ./deploy/monitoring/prom-metrics.yaml:/etc/prometheus/prometheus.yml \
+    0a1bcc4b2d6a
--- trace/hpota/lib/hpota.ex
+++ trace/hpota/lib/hpota.ex
@@ -1,4 +1,23 @@
 defmodule Hpota do
+  use Honey, license: "Dual BSD/GPL"
+
+  defmap(
+    :Count_Sys_Calls_Invoked,
+    %{
+      type: BPF_MAP_TYPE_ARRAY, max_entries: 335, print: true,
+      print_elem: [{"SysRead:", 0}, {"SysWrite:", 1}, {"SysKill:", 62}, {"SysMkdir:", 83}, {"SysGetRandom:", 318}]
+    }
+  )
+
+
+  @sec "tracepoint/raw_syscalls/sys_enter"
+  def main(ctx) do
+    id = ctx.id
+
+    id_count = Honey.Bpf_helpers.bpf_map_lookup_elem(:Count_Sys_Calls_Invoked, id)
+    Honey.Bpf_helpers.bpf_map_update_elem(:Count_Sys_Calls_Invoked, id, id_count + 1)
+    0 # return :ok
+  end
   @moduledoc """
   Documentation for `Hpota`.
   """
