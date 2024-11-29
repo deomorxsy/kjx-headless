a68b68911c2b1540856eaa648474120f9536af9f
asari <32879431+deomorxsy@users.noreply.github.com>
2024-11-29 18:46:18 -0300

    fix: place prom-scraper at the right directory
    fix: place prom-scraper at the right directory
    add: script to operate the  default ebpf-exporter binary

---
+++ scripts/monitor/bin-ebpfexp.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# simply runs the release binary for ebpf_exporter
+# PS: note that to visualize it with grafana you will
+#     need the prometheus scrape server also running.
+
+sudo ./ebpf_exporter.x86_64 \
+    --config.dir=./sample/examples/ \
+    --config.names=biolatency \
+    --debug
