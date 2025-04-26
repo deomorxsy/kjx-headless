babdd6bbf21642b177d03136aa95074f4fbd3b5d
asari <32879431+deomorxsy@users.noreply.github.com>
2025-04-25 22:36:03 -0300

    add: bpf helper start to the libbpfgo project

---
+++ trace/exporter-libbpfgo/bpf_helpers.h
@@ -0,0 +1,12 @@
+#ifndef __BOOTSTRAP_H
+#define __BOOTSTRAP_H
+
+#define TASK_COMM_LEN 16
+#define MAX_FILENAME_LEN 127
+
+struct event {
+    int pid;
+    int ppid
+}
+
+#endif /* end of __BOOTSTRAP_H */
