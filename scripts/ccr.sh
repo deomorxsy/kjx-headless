93553cc58c90c964091581f2346c9d6415be4644
asari <32879431+deomorxsy@users.noreply.github.com>
2025-10-10 21:36:24 -0300

    fix: bump ccr.sh script to newest version with helper functions and pretty print.

--- scripts/ccr.sh
+++ scripts/ccr.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/bash
+#!/usr/bin/sh
 #
 # ccr: check container runtime
-#
+# depends on systemd
 
 registry() {
     #checker
-    exists_command=$(command -v podman &>/dev/null)
+    EXISTS_COMMAND=$( command -v podman >/dev/null 2>&1 )
 
     if [ $? -eq 1 ]; then
         podman run -d -p 5000:5000 --name registry registry:latest
@@ -17,28 +17,6 @@ registry() {
 
     curl -s -i -X GET http://registry.localhost:5000/v2/_catalog | grep mock_ist
 
-}
-
-dddsad(){
-    echo
-}
-
-checker() {
-    exists_wgh=$(which podman 2>&1 | grep -o "no" | head -n 1)
-    exists_command=$(command -v podman &>/dev/null)
-
-    # bash square bracket eval to check for empty expanded variable in a string,
-    # AND if the podman binary have a path.
-    if [ "$exists_command" == '1' ]; then
-        echo 'Using default DOCKER_HOST...'
-        return
-    # if true, command with the v flag outputs a line.
-    elif $exists_command; then
-        echo 'Podman found. Invoking podman_compose...'
-        # hook to set podman service (systemd socket unit file) as DOCKER_HOST
-        podman_compose
-        return
-    fi
 }
 
 podman_compose() {
@@ -61,4 +39,53 @@ printf "\n\n"
 
 #docker compose -f ./oci/container-compose.yml build
 }
+
+checker() {
+    # exists_wgh=$(which podman 2>&1 | grep -o "no" | head -n 1)
+    EXISTS_COMMAND=$( command -v podman >/dev/null 2>&1 )
+
+    # bash square bracket eval to check for empty expanded variable in a string,
+    # AND if the podman binary have a path.
+    if [ "$EXISTS_COMMAND" = '1' ]; then
+        echo 'Using default DOCKER_HOST...'
+        return
+    # if true, command with the v flag outputs a line.
+    elif $EXISTS_COMMAND; then
+        echo 'Podman found. Invoking podman_compose...'
+        # hook to set podman service (systemd socket unit file) as DOCKER_HOST
+        podman_compose
+        return
+    fi
+}
+
+print_usage() {
+cat <<-END >&2
+USAGE: ccr [-options]
+                - checker
+                - version
+                - help
+eg,
+ccr -checker   # runs qemu pointing to a custom initramfs and kernel bzImage
+ccr -version # shows script version
+ccr -help    # shows this help message
+
+See the man page and example file for more info.
+
+END
+
+}
+
+
+# Check the argument passed from the command line
+if [ "$CCR_MODE" = "-checker" ] || [ "$CCR_MODE" = "--checker" ] || [ "$CCR_MODE" = "checker" ]; then
+    checker
+elif [ "$1" = "help" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    print_usage
+elif [ "$1" = "version" ] || [ "$1" = "-v" ] || [ "$1" = "--version" ]; then
+    printf "version"
+else
+    echo "Invalid function name. Please specify one of: function1, function2, function3"
+    print_usage
+fi
+
 
