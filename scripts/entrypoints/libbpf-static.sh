8e993a05f42b606700c964ef90f418da20f809f7
asari <32879431+deomorxsy@users.noreply.github.com>
2025-10-16 15:27:50 -0300

    fix: libbpf-core header includes and build dependencies [1/n]

--- scripts/entrypoints/libbpf-static.sh
+++ scripts/entrypoints/libbpf-static.sh
@@ -3,10 +3,17 @@
 runner() {
 
 CCR_MODE="checker" . ./scripts/ccr.sh && \
-    docker run --rm -it \
-    -v ./trace/libbpf-core/:/app/:ro \
-    --entrypoint=/bin/sh \
+    docker run --rm -it     \
+    -v ./assets/blazesym:/app/assets/blazesym:ro \
+    -v ./assets/bpftool:/app/assets/bpftool:ro \
+    -v ./assets/libbpf:/app/assets/libbpf:ro \
+    -v ./assets/libbpf-bootstrap:/app/assets/libbpf-bootstrap:ro \
+    -v ./trace/libbpf-core:/app/trace/libbpf-core:ro \
+    --entrypoint=/bin/sh    \
     alpine:3.20
+
+
+# -v ./trace/libbpf-core/:/app/:ro \
 }
 
 builder() {
@@ -17,6 +24,60 @@ CCR_MODE="checker" . ./scripts/ccr.sh && \
 # busybox does not have apk. resort to copy dependencies between the root filesystem instead if needed
 # busybox:1.36.1-musl
 
+synch_repos() {
+
+submods='./assets/blazesym
+./assets/bpftool
+./assets/libbpf
+./assets/libbpf-bootstrap
+'
+
+if command -v git > /dev/null; then
+    # traverse the directories and synchronize their main branches
+    printf '%s' "$submods" |
+        while IFS='' read -r path
+        do
+            #echo "$path"
+            cd "$path" || return
+                if ! git pull; then
+                    printf "Error: git was not able to pull the repository. Exiting now...\n"
+                fi
+                printf "\n|> %s was synchronized with success! \o/ \n\n." "$(basename "$path")"
+            cd - > /dev/null || return
+        done
+    printf "\n|> Current dir: %s \n\n" "$(pwd)"
+else
+    printf "\n|> Error: git is not installed. Exiting now...\n\n"
+fi
+
+}
+
+hotpatch() {
+
+    # keep it up to date before applying patches
+    synch_repos
+
+    # Old header
+    OLD_HEADER="./assets/libbpf-bootstrap/libbpf/src/libbpf_internal.h"
+    #OLDH_DEPS="./assets/libbpf-bootstrap/libbpf/src/libbpf.h"
+
+    # New header to be included by libbpf_internal.h
+    NEW_HEADER="./assets/libbpf-bootstrap/libbpf/src/libbpf_internal_SEDADO.h"
+
+    # match and replace
+    # sed -i 's/#include "libbpf.h"/#include "./libbpf.h"/' ./.output/bpftool/bootstrap/libbpf/include/bpf/libbpf_internal.h
+    sed 's/#include "libbpf.h"/#include "./libbpf.h"/' "${OLD_HEADER}" > "${NEW_HEADER}"
+
+    # Create patch
+    diff -Naru "${OLD_HEADER}" "${NEW_HEADER}" > ./trace/libbpf-core/libbpf_internal.patch
+
+
+
+
+    diff -Naru ./assets/libbpf-bootstrap/libbpf/src/libbpf_internal.h ./new > ./file.patch
+
+
+}
 
 print_usage() {
 cat <<-END >&2
