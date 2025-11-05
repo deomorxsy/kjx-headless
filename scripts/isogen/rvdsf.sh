c0d5e627d18927514b8da1ec45929b4f7930cc55
asari <32879431+deomorxsy@users.noreply.github.com>
2025-11-05 10:25:52 -0300

    fix: add print usage and fix other typos

--- scripts/isogen/rvdsf.sh
+++ scripts/isogen/rvdsf.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# useful functions for handling
+# raw virtual disk sparse files
 
 sparseFile() {
     SPARSE="./utils/storage/eulab-hd"
@@ -7,7 +9,7 @@ sparseFile() {
     mkfs.ext4 $SPARSE
 }
 
-virtstoraged() {
+virtStoraged() {
     QCOW_FILE="./utils/storage/eulab.qcow2"
 
     if [ ! -e $QCOW_FILE ]; then
@@ -20,3 +22,40 @@ virtstoraged() {
     fi
 
 }
+
+print_usage() {
+cat <<-END >&2
+USAGE: rvdsf [-options]
+                - sparseFile
+                - virtStoraged
+                - help
+                - version
+eg,
+rvdsf -thirdver   # runs qemu pointing to a custom initramfs and kernel bzImage
+rvdsf -dropbear  # runs qemu enabled with ssh for quick file copying between target vm and host
+rvdsf -airgap  # runs qemu with files to run k3s air-gapped
+rvdsf -debug # the same of thirdver but with serial and pty flags for kernel debug
+rvdsf -help    # shows this help message
+rvdsf -version # shows script version
+
+See the man page and example file for more info.
+
+END
+
+}
+
+
+# Check the argument passed from the command line
+if [ "$MODE" = "-sf" ] || [ "$MODE" = "--sf" ] || [ "$MODE" = "--sparsefile" ] ; then
+    sparseFile
+elif [ "$MODE" = "-vs" ] || [ "$MODE" = "--vs" ] || [ "$MODE" = "--virtstoraged" ] ; then
+    virtStoraged
+elif [ "$1" = "help" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    print_usage
+elif [ "$1" = "version" ] || [ "$1" = "-v" ] || [ "$1" = "--version" ]; then
+    printf "\n|> kjx-headless/rvdsf version: 1.0.0\n\n"
+else
+    echo "Invalid function name. Please specify one of: function1, function2, function3"
+    print_usage
+
+fi
