38b87465a4f0ef463b59005ffc3ed325f657d8f1
asari <32879431+deomorxsy@users.noreply.github.com>
2025-11-05 22:50:06 -0300

    trace/libbpf-core [1/n]
    feat: add up-to-date code for runqlat and friend scripts
    to work out of the box with the libbpf-bootstrap Makefile.
    
    todo: fix builds, merge final and final/.. programs into
    only one directory

---
+++ trace/libbpf-core/cpu.h
@@ -0,0 +1,25 @@
+#include "vmlinux.h" // Or wherever your vmlinux.h is located
+#include <bpf/bpf_helpers.h> // Common BPF helpers
+// ... other includes if needed
+
+#ifndef __BOOTSTRAP_H
+#define __BOOTSTRAP_H
+
+#define TASK_COMM_LEN 16
+#define MAX_FILENAME_LEN 127
+
+//@[kstack, comm] are values of type count()
+//@[bytes] are values of type hist()
+struct event {
+    int pid;
+    int ppid;
+    unsigned exit_code;
+    unsigned long long duration_ns;
+    char comm[TASK_COMM_LEN];
+    char filename[MAX_FILENAME_LEN];
+    bool exit_event;
+    __uint tgid;
+};
+
+
+#endif /*__BOOTSTRAP_H*/
