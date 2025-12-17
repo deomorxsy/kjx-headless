5ed8d7af2be05b76f5946a360e05523bd41ad856
asari <32879431+deomorxsy@users.noreply.github.com>
2025-12-17 16:09:21 -0300

    fix: rename the bootscript to a more adequate filename due to the actual scope of the POC. In this state, the full-blown distro does not need to be ready yet: it is just to Proof the Concept for any cited artifact.

--- scripts/isogen/poc-bootscript.sh
+++ scripts/isogen/poc-bootscript.sh
@@ -78,13 +78,12 @@ ln -s "/bin/kmod" "/bin/modinfo"
 ln -s "/bin/kmod" "/bin/modprobe"
 ln -s "/bin/kmod" "/bin/depmod"
 
-ln -s "/bin/lsmod"     "/sbin/lsmod"
-ln -s "/bin/rmmod"     "/sbin/rmmod"
-ln -s "/bin/insmod"    "/sbin/insmod"
-ln -s "/bin/modinfo"   "/sbin/modinfo"
-ln -s "/bin/modprobe"  "/sbin/modprobe"
-ln -s "/bin/depmod"    "/sbin/depmod"
-
+ln -s "/bin/lsmod" "/sbin/lsmod"
+ln -s "/bin/rmmod" "/sbin/rmmod"
+ln -s "/bin/insmod" "/sbin/insmod"
+ln -s "/bin/modinfo" "/sbin/modinfo"
+ln -s "/bin/modprobe" "/sbin/modprobe"
+ln -s "/bin/depmod" "/sbin/depmod"
 
 # Prepare run directory for containerd and k3s
 mkdir -p /run /var/run
@@ -99,15 +98,125 @@ mkdir -p /etc/rancher/k3s
 # originally located on the server at: /var/lib/rancher/k3s/server/etc/crictl.yaml
 # originally located on the server at: /var/lib/rancher/k3s/agent/etc/crictl.yaml
 (
-cat <<EOF
+    cat <<EOF
 runtime-endpoint: unix:///run/k3s/containerd/containerd.sock
 EOF
 ) | tee /app/crictl.yaml
 
+# ===============================================
+#
+# runtimeClass setup for k3s
+(
+    cat <<EOF
+# Paths to look for a valid OCI runtime (crun, runc, kata, runsc, krun, etc)
+[engine.runtimes]
+crun = [
+  "/usr/bin/crun",
+  "/usr/sbin/crun",
+  "/usr/local/bin/crun",
+  "/usr/local/sbin/crun",
+  "/sbin/crun",
+  "/bin/crun",
+  "/run/current-system/sw/bin/crun",
+]
+
+# crun-vm is an OCI Runtime that enables Podman,
+# Docker, and Kubernetes to run QEMU-compatible
+# Virtual Machine (VM) images.
+crun-vm = [
+  "/usr/bin/crun-vm",
+  "/usr/local/bin/crun-vm",
+  "/usr/local/sbin/crun-vm",
+  "/sbin/crun-vm",
+  "/bin/crun-vm",
+  "/run/current-system/sw/bin/crun-vm",
+]
+
+kata = [
+  "/usr/bin/kata-runtime",
+  "/usr/sbin/kata-runtime",
+  "/usr/local/bin/kata-runtime",
+  "/usr/local/sbin/kata-runtime",
+  "/sbin/kata-runtime",
+  "/bin/kata-runtime",
+  "/usr/bin/kata-qemu",
+  "/usr/bin/kata-fc",
+]
+
+runc = [
+  "/usr/bin/runc",
+  "/usr/sbin/runc",
+  "/usr/local/bin/runc",
+  "/usr/local/sbin/runc",
+  "/sbin/runc",
+  "/bin/runc",
+  "/usr/lib/cri-o-runc/sbin/runc",
+]
+
+runsc = [
+  "/usr/bin/runsc",
+  "/usr/sbin/runsc",
+  "/usr/local/bin/runsc",
+  "/usr/local/sbin/runsc",
+  "/bin/runsc",
+  "/sbin/runsc",
+  "/run/current-system/sw/bin/runsc",
+]
+
+youki = [
+  "/usr/local/bin/youki",
+  "/usr/bin/youki",
+  "/bin/youki",
+  "/run/current-system/sw/bin/youki",
+]
+
+krun = [
+  "/usr/bin/krun",
+  "/usr/local/bin/krun",
+]
+
+EOF
+) | tee "$ISO_DIR"/rootfs/etc/containers/containers.conf
+
+# Setup storage info for containers
+# this need user management enabled
+(
+    cat <<EOF
+[storage]
+driver = "overlay"
+
+# Default Storage Driver, Must be set for proper operation.
+driver = "overlay"
+
+# Temporary storage location
+runroot = "/run/containers/storage"
+graphroot = "/var/lib/containers/storage"
+
+# Storage path for rootless users
+rootless_storage_path = "$HOME/.local/share/containers/storage"
+
+[storage.options]
+pull_options = {enable_partial_images = "false", use_hard_links = "false", ostree_repos=""}
+additionalimagestores = [
+]
+
+[storage.options.overlay]
+
+# Path to an helper program to use for mounting the file system instead of mounting it
+# directly.
+mount_program = "/usr/bin/fuse-overlayfs"
+
+# mountopt specifies comma separated list of extra mount options
+mountopt = "nodev"
+
+
+EOF
+) | tee /etc/containers/storage.conf
+
 # ======
 # Setup the crictl configuration file: crictl.yaml
 (
-cat <<EOF
+    cat <<EOF
 
 runtime-endpoint: unix:///run/k3s/containerd/containerd.sock
 image-endpoint: unix:///run/k3s/containerd/containerd.sock
@@ -130,7 +239,6 @@ ls -allhtr /mnt/airgap-registry-image
 unsquashfs -d ../k3s-squashfs/ ./k3s-tarball.squashfs
 
 cd - || return
-
 
 # Setup bpftrace
 # bpftrace dependencies, libclang from llvm17
@@ -159,7 +267,7 @@ ln -s /run/containerd/containerd.sock /run/k3s/containerd/
 #k3s server --default-runtime=runc --disable=traefik & > /dev/null 2>&1
 
 (
-cat <<EOF
+    cat <<EOF
 containerd:
   snapshotter: fuse-overlayfs
 EOF
@@ -169,16 +277,15 @@ EOF
 
 #k3s server --default-runtime=runc --disable=traefik --config=/etc/rancher/k3s/agent-config.yaml & > /dev/null 2>&1
 
-
 # k3s server --disable-agent --default-runtime=runc --disable=traefik --snapshotter=fuse-overlayfs > /dev/null 2>&1 &
 
-k3s server --disable-agent --default-runtime="crun" --disable=traefik --snapshotter=fuse-overlayfs > /dev/null 2>&1 &
+k3s server --disable-agent --default-runtime="crun" --disable=traefik --snapshotter=fuse-overlayfs >/dev/null 2>&1 &
 
 # k3s server --disable-agent --default-runtime="crun" --disable=traefik --snapshotter=overlayfs > /dev/null 2>&1 &
 
 GETK3S_PID=$(pgrep k3s)
 (
-cat <<EOF
+    cat <<EOF
 #!/bin/sh
 
 bpftrace -e 'profile:hz:49 /pid == ${GETK3S_PID}/ { @[ustack] = count(); }' \
@@ -222,21 +329,19 @@ k3s ctr images ls
 # verify if image is imported
 # k3s ctr -n k8s.io images ls
 
-
 # if pgrep k3s; then
 #     kill -SIGTERM "$GET_BPFTRACE_PID"
 # fi
 # create the container with k3s ctr,
 # which will be run by k3s containerd
 k3s ctr -n="k8s.io" run --rm -t \
-  localhost:5000/registry:3.0 \
-  registry-test \
-  /entrypoint.sh
+    localhost:5000/registry:3.0 \
+    registry-test \
+    /entrypoint.sh
 
 # Check metrics-server state
 CHECK_POD_NAME="$(k3s kubectl get pods -n=kube-system | grep "metrics-server" | awk '{ print $1 }')"
 k3s kubectl describe pod "$CHECK_POD_NAME" -n=kube-system
-
 
 # (
 # cat <<EOF
@@ -254,11 +359,9 @@ k3s kubectl describe pod "$CHECK_POD_NAME" -n=kube-system
 # EOF
 # )
 
-
-
 mkdir -p /app/piest.yaml
 (
-cat <<PIEST
+    cat <<PIEST
 apiVersion: v1
 kind: Pod
 metadata:
@@ -277,7 +380,7 @@ spec:
   restartPolicy: Never
 
 PIEST
- ) | tee ./app/piest.yaml
+) | tee ./app/piest.yaml
 
 # Gracefully exit bpftrace and return plot graph
 kill -SIGINT "$BPFTRACE_PID"
@@ -285,7 +388,6 @@ kill -SIGINT "$BPFTRACE_PID"
 # Kill k3s
 #
 kill -SIGTERM $(pgrep k3s)
-
 
 # reboot: power down
 # stops recording the asciinema section
