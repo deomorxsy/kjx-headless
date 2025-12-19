314061705524270d617da44a99b1d8a86494ef29
asari <32879431+deomorxsy@users.noreply.github.com>
2025-12-19 15:23:11 -0300

    feat: add error handling into the function calls of the ./scripts/isogen/main.sh shellcript.

--- scripts/isogen/main.sh
+++ scripts/isogen/main.sh
@@ -1,37 +1,97 @@
 #!/bin/sh
 
 # 1st STEP: set variables
-MODE="-setvars" . ./scripts/isogen/set-vars.sh
+if ! (MODE="-setvars" . ./scripts/isogen/set-vars.sh); then
+    echo "|> Error: it was not possible to run the set variables script. Exiting now..."
+    echo && echo
+    return 1
+fi
+echo "|> Successfully ran the set variables script."
 
 # 2nd STEP: scaffolding
-MODE="-scaff" . ./scripts/isogen/scaffolding.sh
+if ! (MODE="-scaff" . ./scripts/isogen/scaffolding.sh); then
+    echo "|> Error: it was not possible to run the scaffolding script. Exiting now..."
+    echo && echo
+    return 1
+fi
+echo "|> Successfully ran the scaffolding script."
 
 # 3rd STEP: Retrieve dropbear-based ssh-enabled-rootfs artifact from previous actions workflow
-MODE="-rootafail" . ./scripts/isogen/rootfs.sh
+if ! (MODE="-rootafail" . ./scripts/isogen/rootfs.sh); then
+    echo "|> Error: it was not possible to run the rootfs script. Exiting now..."
+    echo && echo
+    return 1
+fi
+echo "|> Successfully ran the rootfs script."
 
 # 4th STEP: Retrieve runit service tree
-MODE="-itarun" . ./scripts/isogen/runit.sh
+if ! (MODE="-itarun" . ./scripts/isogen/runit.sh); then
+    echo "|> Error: it was not possible to run the runit service tree script. Exiting now..."
+    echo && echo
+    return 1
+fi
+echo "|> Successfully ran the runit service tree script."
 
 # 5th STEP: set OCI-CRI container definitions
-MODE="-setacontainers" . ./scripts/isogen/oci-cri.sh
+if ! (MODE="-setacontainers" . ./scripts/isogen/oci-cri.sh); then
+    echo "|> Error: it was not possible to run the script to set the OCI-CRI container definitions. Exiting now..."
+    echo && echo
+    return 1
+fi
+echo "|> Successfully ran the script to set the OCI-CRI container definitions."
 
 # 6th STEP: Retrieve qonq-qdb packaging (shadow + iptables goes here)
-MODE="-packaja" . ./scripts/isogen/packaging.sh
+if ! (MODE="-packaja" . ./scripts/isogen/packaging.sh); then
+    echo "|> Error: it was not possible to run the qonq-qdb packaging (shadow+iptables goes here). Exiting now..."
+    echo && echo
+    return 1
+fi
+echo "|> Successfully ran the qonq-qdb packaging (shadow+iptables goes here)."
 
-# 7th STEP: Organize the squashfs logic
-MODE="-squasha" . ./scripts/isogen/squasha.sh
+# 7th STEP: Organize the squashfs filesystem logic
+if ! (MODE="-squasha" . ./scripts/isogen/squasha.sh); then
+    echo "|> Error: it was not possible to run the squashfs filesystem logic script. Exiting now..."
+    echo && echo
+    return 1
+fi
+echo "|> Successfully ran the squashfs filesystem logic script."
 
 # 8th STEP: Retrieve bzImage artifact from previous actions workflow
-MODE="-buildakernel" . ./scripts/isogen/bzImage.sh
+if ! (MODE="-buildakernel" . ./scripts/isogen/bzImage.sh); then
+    echo "|> Error: it was not possible to run the bzImage generation script (kernel build). Exiting now..."
+    echo && echo
+    return 1
+fi
+echo "|> Successfully ran the bzImage generation script (kernel build)."
 
-# 9th STEP: Retrieve initramfs artifact from previous actions workflow
-MODE="-inita" . ./scripts/isogen/initramfs.sh
+# 9th STEP: Retrieve initramfs artifact.
+if ! (MODE="-inita" . ./scripts/isogen/initramfs.sh); then
+    echo "|> Error: it was not possible to run the script to retrieve the initramfs artifact. Exiting now..."
+    echo && echo
+    return 1
+fi
+echo "|> Successfully ran the script to retrieve the initramfs artifact."
 
 # 10th STEP: Retrieve beetor_bwc signal-based tracing orchestration from previous actions workflow
-MODE="-sting" . ./scripts/isogen/beetor.sh
+if ! (MODE="-sting" . ./scripts/isogen/beetor.sh); then
+    echo "|> Error: it was not possible to run the script to retrieve beetor_bwc artifact (for signal-based tracing orchestration). Exiting now...."
+    echo && echo
+    return 1
+fi
+echo "|> Successfully ran the script to retrieve beetor_bwc artifact (for signal-based tracing orchestration)."
 
 # 11th STEP: Bootloaders setup
-MODE="-bootaeloada" . ./scripts/isogen/bootloaders.sh
+if ! (MODE="-bootaeloada" . ./scripts/isogen/bootloaders.sh); then
+    echo "|> Error: it was not possible to run the script to setup the bootloaders for the final iso. Exiting now..."
+    echo && echo
+    return 1
+fi
+echo "|> Successfully ran the script to setup the bootloaders for the final iso."
 
 # 12th STEP: Build ISO9660
-MODE="-isaisa" . ./scripts/isogen/iso9660.sh
+if ! (MODE="-isaisa" . ./scripts/isogen/iso9660.sh); then
+    echo "|> Error: it was not possible to run the build the iso9660 shellscript. Exiting now..."
+    echo && echo
+    return 1
+fi
+echo "|> Successfully ran the build the iso9660 shellscript."
