dbc4c901c69e121c3bbc3bb2400cf9f1209a799a
asari <32879431+deomorxsy@users.noreply.github.com>
2025-12-23 23:53:57 -0300

    fix: add logic to build, push and retrieve artifacts from the build of iptables which configures tc, basic firewall and netfilter network. workflow: Makefile -> ./scripts/packages/usgp-man.sh -> ./scripts/packages/Dockerfile.iptables.qonq -> ./scripts/packages/iptables-setup.sh

--- scripts/packages/usgp-man.sh
+++ scripts/packages/usgp-man.sh
@@ -1,48 +1,159 @@
 #!/bin/sh
 # usgp-man: user-group management
 #
+# packaging variables
+PACKAGING_ART_DIR="./artifacts/packaging"
 
-SHADOW_PKG_URI="https://github.com/shadow-maint/shadow/releases/download/4.18.0/shadow-4.18.0.tar.xz"
+SHADOW_SO_PKG="./artifacts/packaging/shadow-so-pkg.tar.gz"
+SHADOW_BIN_PKG="./artifacts/packaging/shadow-so-pkg.tar.gz"
+export SHADOW_SO_PKG SHADOW_BIN_PKG
 
-set_network() {
+# iptables shared objects and dynamically linked binaries
+IPTABLES_SO_PKG="./artifacts/packaging/iptables-so-pkg.tar.gz"
+IPTABLES_BIN_PKG="./artifacts/packaging/iptables-so-pkg.tar.gz"
+export IPTABLES_SO_PKG IPTABLES_BIN_PKG
 
-    CCR_MODE="-checker" . ./scripts/ccr.sh && \
-        docker compose -f ./compose.yml --progress=plain build --no-cache qonq-iptables
+# podman shared objects and dynamically linked binaries
+PODMAN_SO_PKG="./artifacts/packaging/podman-so-pkg.tar.gz"
+PODMAN_BIN_PKG="./artifacts/packaging/podman-bin-pkg.tar.gz"
+export PODMAN_SO_PKG PODMAN_BIN_PKG
+
+# bpftrace shared objects and dynamically linked binaries
+BPFTRACE_SO_PKG=""
+BPFTRACE_BIN_PKG=""
+export BPFTRACE_SO_PKG BPFTRACE_BIN_PKG
+
+# build from source URIs
+BFS_SHADOW_PKG_URI="https://github.com/shadow-maint/shadow/releases/download/4.18.0/shadow-4.18.0.tar.xz"
+export BFS_SHADOW_PKG_URI
+
+set_iptables() {
+
+    # CCR_MODE="-checker" . ./scripts/ccr.sh &&
+    #     docker compose -f ./compose.yml --progress=plain build --no-cache qonq_iptables
+    #
+
+    # start OCI registry server
+    if ! podman start registry; then
+        echo "|> Error: could not start OCI registry sever. Attempting to run the image..."
+        echo && echo
+        #return 1
+
+        # run the registry:3.0 container image.
+        if ! (podman run -d -p 5000:5000 --name registry registry:3.0); then
+            echo "|> Error: could not run the registry:3.0 container image. Exiting now..."
+            echo && echo
+            return 1
+        fi
+        echo "|> Ran the OCI registry server with success. Proceeding..."
+        echo && echo
+    fi
+    echo "|> OCI registry server started with success"
+
+    # check if the image already exists
+    if ! (podman images | grep "localhost:5000/qonq_iptables" | awk '{print $1}'); then
+        echo "|> Error: could not find the localhost:5000/qonq_iptables image at the OCI registry:3.0 server. Attempting to build now..."
+        echo && echo
+        # return 1
+        # Build the iptables container with ccr.sh to use  Podman Service as the compose tool
+        if ! CCR_MODE="-checker" . ./scripts/ccr.sh && docker compose -f ./compose.yml --progress=plain build --no-cache qonq_iptables; then
+            echo "|> Error: could not run the ccr.sh script for Podman Service as the compose tool. Exiting now..."
+            echo && echo
+            return 1
+
+        fi
+        echo "|> Build the [qonq_iptables container with ccr.sh to use Podman Service as the compose tool with success. Proceeding..."
+        echo && echo
+
+        # push built image into the registry:3.0 localhost:5000 server container.
+        if ! podman push localhost:5000/qonq_iptables:latest; then
+            echo "|> Error: could not push the built [qonq_iptables] image into the OCI registry:3.0 localhost:5000 server container. Exiting now..."
+            echo && echo
+            return 1
+        fi
+        echo "|> Pushed built image into the OCI registry:3.0 localhost:5000 server container. Proceeding..."
+        echo && echo
+    fi
+    echo "|> [qonq_iptables] image found at the localhost:5000/qonq_iptables OCI registry:3.0 server. Proceeding..."
+
+    # Create the built qonq_iptables container
+    # podman run -it --name qonq_iptables -d localhost:5000/qonq_iptables:latest
+    if ! (CCR_MODE="-checker" . ./scripts/ccr.sh && docker compose -f ./compose.yml create qonq_iptables); then
+        echo "|> Error: could not create the built [qonq_iptables] container using the ccr.sh script to use Podman Service as the compose tool"
+        echo && echo
+        return 1
+    fi
+    echo "|> Created the built [qonq_iptables] container using the ccr.sh script to use Podman Service as the compose tool with success. Proceeding..."
+    echo && echo
+
+    # check created containers
+    CCR_MODE="-checker" . ./scripts/ccr.sh && docker compose ps --all
+
+    # check for the qonq_iptables image at localhost:5000/qonq_iptables
+    podman images | grep "localhost:5000/qonq_iptables" | awk '{print $1}'
+
+    # copy qonq_iptables tarball into the ./artifacts/microvms directory.
+    mkdir -p "${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]}"
+    if ! podman cp qonq_iptables:/app/iptables-so-pkg.tar.gz "${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]}"; then
+        echo "|> Error: could not copy the iptables shared objects tarball to the PACKAGING_ART_DIR=${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]} filepath. Exiting now..."
+        return 1
+    fi
+    echo "|> Copied the iptables shared objects tarball into the PACKAGING_ART_DIR=${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]} filepath with success. Proceeding... "
+
+    mkdir -p "${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]}"
+    if ! podman cp qonq_iptables:/app/iptables-bin-pkg.tar.gz "${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]}"; then
+        echo "|> Error: could not copy the [qonq_iptables] dynamically linked binaries tarball to the PACKAGING_ART_DIR=${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]} filepath. Exiting now..."
+        return 1
+    fi
+    echo "|> Copied the [qonq_iptables] dynamically linked binaries tarball into the PACKAGING_ART_DIR=${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]} filepath with success. Proceeding... "
+    # docker cp qonq_iptables:/app/iptables-so-pkg.tar.gz "${{ github.workspace }}"/artifacts/packaging/
+    # docker cp qonq_iptables:/app/iptables-bin-pkg.tar.gz "${{ github.workspace }}"/artifacts/packaging/
+
+    # Stop container registry
+    if ! (podman stop registry); then
+        echo "|> Error: could not stop the OCI registry server! Exiting now..."
+        echo && echo
+        return 1
+    fi
+    echo "|> Successfully stopped the OCI registry server."
+    echo && echo
 
 }
 
 set_shadow() {
 
-    CCR_MODE="-checker" . ./scripts/ccr.sh && \
-        docker compose -f ./compose.yml --progress=plain build --no-cache qonq-shadow
+    CCR_MODE="-checker" . ./scripts/ccr.sh &&
+        docker compose -f ./compose.yml --progress=plain build --no-cache qonq_shadow
 
-echo
+    echo
 }
 
 main_usgp_man() {
 
-if ! set_shadow; then
-    return 1
-fi
-echo "|> shadow setup with success."
+    if ! set_shadow; then
+        return 1
+    fi
+    echo "|> shadow setup with success."
 }
 
 print_usage() {
-cat <<-END >&2
+    cat <<-END >&2
 USAGE: usgp-man [-options]
                 - shadow
-                - firecracker
-                - gvisor
-                - kata
+                - iptables
+                - podman
+                - bpftrace
+                - all
                 - version
                 - help
 eg,
-MODE="all"          ./usgp-man.sh   # Fetch all default dependencies
-MODE="shadow"       ./usgp-man.sh   # Setup firecracker as main microvm
-MODE="network"      ./usgp-man.sh   # Setup gvisor as main microvm
-MODE="kata"         ./usgp-man.sh   # Setup kata-containers as main microvm
-MODE="version"      ./usgp-man.sh   # shows script version
-MODE="help"         ./usgp-man.sh   # shows this help message
+MODE="all"          ./usgp-man.sh   # Setup all default packaging
+MODE="shadow"       ./usgp-man.sh   # Setup shadow packaging
+MODE="network"      ./usgp-man.sh   # Setup iptables, tc and netfilter packaging
+MODE="podman"       ./usgp-man.sh   # Setup podman HLCR packaging
+MODE="bpftrace"     ./usgp-man.sh   # Setup bpftrace packaging
+MODE="version"      ./usgp-man.sh   # Shows script version
+MODE="help"         ./usgp-man.sh   # Shows this help message
 
 See the man page and example file for more info.
 
@@ -50,26 +161,25 @@ END
 
 }
 
-
 # Check the argument passed from the command line
-if ! [ -z "${MODE}" ] && \
-    [ "${MODE}" = "shadow" ] || \
-    [ "${MODE}" = "network" ] || \
+if ! [ -z "${MODE}" ] &&
+    [ "${MODE}" = "shadow" ] ||
+    [ "${MODE}" = "iptables" ] ||
     [ "${MODE}" = "all" ]; then
     case "${MODE}" in
-        "all")
-            main_usgp_man
-            ;;
-        "shadow")
-            set_shadow
-            ;;
-        "network")
-            set_iptables # iptables, conntrack, etc
-            ;;
-        *)
-            echo "Invalid microvm. Please specify one of: firecracker, gvisor, kata"
-            print_usage
-            ;;
+    "all")
+        main_usgp_man
+        ;;
+    "shadow")
+        set_shadow
+        ;;
+    "iptables")
+        set_iptables # iptables, conntrack, etc
+        ;;
+    *)
+        echo "Invalid packaging option. Please specify one of: shadow, iptables, all"
+        print_usage
+        ;;
     esac
 
 elif [ "${MODE}" = "help" ] || [ "${MODE}" = "-h" ] || [ "${MODE}" = "--help" ]; then
@@ -80,5 +190,3 @@ else
     echo "Invalid function name. Please specify one of the available functions:"
     print_usage
 fi
-
-
