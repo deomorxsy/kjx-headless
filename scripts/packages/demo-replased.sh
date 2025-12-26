1633e8748bce73be217de003a6aaf6e4dea2e3e8
asari <32879431+deomorxsy@users.noreply.github.com>
2025-12-25 22:27:23 -0300

    fix: add another error handling for the last loop with ldd as well as pretty-printing of the scope to the [./scripts/packages/demo-replased.sh]

--- scripts/packages/demo-replased.sh
+++ scripts/packages/demo-replased.sh
@@ -8,22 +8,34 @@ if ! (for f in $PODMAN_PKGDEPS_PLACEHOLDER; do
     echo "$f" >>/foo.txt
 done); then
     echo "|> Error: it was not possible to redirect the filepath of dotfiles for the [PLACEHOLDER] apk package. Exiting now..."
+    echo "|> SCOPE: [DEMO_REPLASED]; check: 01"
     return 1
 fi
 echo "|> Successfully redirected the filepath of dotfiles for the [PLACEHOLDER] apk package. Proceeding..."
+echo "|> SCOPE: [DEMO_REPLASED]; check: 01"
 
 # redirect filepath of dynamically linked binary dependencies (shared objects)
 if ! (
     ldd "$(readlink -f "$(apk info -L placeholder | awk 'NR > 1')")" | awk '{print $3}' >>foo.txt
 ); then
     echo "|> Error: it was not possible to redirect the filepath of [PLACEHOLDER] dynamically linked binary dependencies (shared objects). Exiting now..."
+    echo "|> SCOPE: [DEMO_REPLASED]; check: 02"
     return 1
 fi
-echo "|> Successfully redirected the filepath of [PLACEHOLDER] dynamically linked binary dependencies (shared objects). Proceeding..."
+echo "|> Successfully redirected the filepath of [placeholder] dynamically linked binary dependencies (shared objects). Proceeding..."
+echo "|> SCOPE: [DEMO_REPLASED]; check: 02"
 
 #
-for f in /bin/* /usr/bin/* /usr/sbin/*; do
-    case $f in
-    */placeholder) ldd "$(readlink -f "$(which "$f")")" | awk '{print $3}' >>/foo.txt ;;
-    esac
-done
+if ! (
+    for f in /bin/* /usr/bin/* /usr/sbin/*; do
+        case $f in
+        */placeholder) ldd "$(readlink -f "$(which "$f")")" | awk '{print $3}' >>/foo.txt ;;
+        esac
+    done
+) then
+    echo "|> Error: could not find binaries of [placeholder] at [/bin, /usr/bin or /usr/sbin] to ldd (list dynamic dependencies). Exiting now..."
+    echo "|> SCOPE: [DEMO_REPLASED]; check: 03"
+    return 1
+fi
+echo "|> Error: could not find binaries of [placeholder] at [/bin, /usr/bin or /usr/sbin] to ldd (list dynamic dependencies). Exiting now..."
+echo "|> SCOPE: [DEMO_REPLASED]; check: 03"
