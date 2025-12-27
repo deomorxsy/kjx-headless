e4c8d1861ffd41dad398518b5661c2032c8c6b0b
asari <32879431+deomorxsy@users.noreply.github.com>
2025-12-27 13:26:35 -0300

    fix: adjust global environment variables name to rename due the possibility of tampering with the export in ./scripts/sandbox/run-qemu.sh; fix: call individual functions for each packaging option in the main function instead of dump every option in the [poc_packaging]; fix: add pretty-printing and error handling for the poc_pack functions

--- scripts/isogen/packaging.sh
+++ scripts/isogen/packaging.sh
@@ -1,30 +1,39 @@
 #!/bin/sh
 
-poc_packaging() {
+VIRTIO_PASSTHRU_DIR="/mnt/virtio-test"
+export VIRTIO_PASSTHRU_DIR
 
-    VIRTIO_PASSTHRU_DIR="/mnt/virtio-test"
-    export VIRTIO_PASSTHRU_DIR
-    # from ./scripts/isogen/poc-bootscript.sh
+# from ./scripts/isogen/poc-bootscript.sh
 
-    # ======================
-    # Packaging or INFRA artifacts
-    # ======================
-    #
-    # from ./scripts/sandbox/run-qemu.sh
-    #
+# ======================
+# Packaging or INFRA artifacts
+# ======================
+#
+# adapted from ./scripts/sandbox/run-qemu.sh
+#
 
-    # Microvm artifact variables
-    MICROVM_GVISOR_TARBALL="/mnt/virtio-test/gvisor-core.tar.gz"
-    MICROVM_FIRECRACKER_TARBALL="/mnt/virtio-test/firecracker-containerd.tar.gz"
-    MICROVM_KATA_TARBALL="/mnt/virtio-test/kata-containerd.tar.gz"
+# Microvm artifact variables
+MICROVM_GVISOR_POCPACKA="/mnt/virtio-test/gvisor-core.tar.gz"
+MICROVM_FIRECRACKER_POCPACKA="/mnt/virtio-test/firecracker-containerd.tar.gz"
+MICROVM_KATA_POCPACKA="/mnt/virtio-test/kata-containerd.tar.gz"
+export MICROVM_GVISOR_POCPACKA
+export MICROVM_FIRECRACKER_POCPACKA
+export MICROVM_KATA_POCPACKA
 
-    # Tracers artifact variables
-    TRACERS_BPFTRACE_TARBALL="/mnt/virtio-test/bpftrace-tarball-pkg.tar.gz"
+# Tracers artifact variables
 
-    # User Management artifacts
-    PACKAGING_IPTABLES_TARBALL="/mnt/virtio-test/iptables-tarball-pkg.tar.gz"
-    PACKAGING_SHADOW_TARBALL="/mnt/virtio-test/shadow-tarball-pkg.tar.gz"
+TRACERS_BPFTRACE_POCPACKA="/mnt/virtio-test/bpftrace-tarball-pkg.tar.gz"
+export TRACERS_BPFTRACE_POCPACKA
 
+# User Management artifacts
+PACKAGING_IPTABLES_POCPACKA="/mnt/virtio-test/iptables-tarball-pkg.tar.gz"
+PACKAGING_SHADOW_POCPACKA="/mnt/virtio-test/shadow-tarball-pkg.tar.gz"
+PACKAGING_QEMUKJX_POCPACKA="/mnt/virtio-test/qemukjx-tarball-pkg.tar.gz"
+export PACKAGING_IPTABLES_POCPACKA
+export PACKAGING_SHADOW_POCPACKA
+export PACKAGING_QEMUKJX_POCPACKA
+
+poc_pack_bpftrace() {
     #    ;, [27/12/2025 08:41]
     # Packaging and installing bpftrace
     mkdir -p /outro/bpftrace
@@ -42,6 +51,9 @@ poc_packaging() {
     echo "|> Sucessfully copied [/outro/bpftrace/usr/*] into [/usr/]. Exiting now..."
     bpftrace -h
 
+}
+
+poc_pack_iptables() {
     # ;, [27/12/2025 08:47]
     # Packaging and installing iptables
     mkdir -p /outro/iptables
@@ -66,6 +78,66 @@ poc_packaging() {
     fi
     echo "|> Sucessfully copied [/outro/iptables/usr/*] into  [/usr/]. Proceeding..."
     iptables -h
+
+}
+
+poc_pack_qemukjx() {
+
+    mkdir -p /outro/qemukjx
+    if ! (tar -xvf "${VIRTIO_PASSTHRU_DIR:-[EMPTY_VARIABLE]}/qemukjx-tarball-pkg.tar.gz" -C /outro/qemukjx/); then
+        echo "|> Error: could not decompress the [iptables-tarball-pkg.tar.gz]. Exiting now...  "
+        return 1
+    fi
+    echo "|> Sucessfully decompressed the [qemukjx-tarball-pkg.tar.gz]. Proceeding..."
+    ls -l /outro/qemukjx/
+    # if ! (cp -r /outro/iptables/bin/* /bin/); then
+    #     echo "|> Error: could not copy [/outro/iptables/bin/*] into  [/bin/]. Exiting now..."
+    #     return 1
+    # fi
+    if ! (cp -r /outro/qemukjx/etc/* /etc/); then
+        echo "|> Error: could not copy [/outro/iptables/etc/*] into  [/etc/]. Exiting now..."
+        return 1
+    fi
+    echo "|> Sucessfully copied [/outro/qemukjx/etc/*] into  [/etc/]. Proceeding..."
+    if ! (cp -r /outro/qemukjx/usr/* /usr/); then
+        echo "|> Error: could not copy [/outro/qemukjx/usr/*] into  [/usr/]. Exiting now..."
+        return 1
+    fi
+    echo "|> Sucessfully copied [/outro/qemukjx/usr/*] into  [/usr/]. Proceeding..."
+
+    if ! (cp -r /outro/qemukjx/bin/udevadm /bin/); then
+        echo "|> Error: could not copy [/outro/qemukjx/bin/udevadm] into  [/bin/]. Exiting now..."
+        return 1
+    fi
+    echo "|> Sucessfully copied [/outro/qemukjx/bin/udevadm] into  [/bin/udevadm]. Proceeding..."
+
+    if ! (cp -r /outro/qemukjx/sbin/* /sbin/); then
+        echo "|> Error: could not copy [/outro/qemukjx/sbin/*] into  [/sbin/]. Exiting now..."
+        return 1
+    fi
+    echo "|> Sucessfully copied [/outro/qemukjx/sbin/*] into  [/sbin/]. Proceeding..."
+
+}
+
+poc_packaging() {
+
+    if ! poc_pack_bpftrace; then
+        echo "|> Error: could not run [poc_pack_bpftrace]. Exiting now..."
+        return 1
+    fi
+    echo "|> Sucessfully ran [poc_pack_bpftrace]. Proceeding..."
+
+    if ! poc_pack_iptables; then
+        echo "|> Error: could not run [poc_pack_iptables]. Exiting now..."
+        return 1
+    fi
+    echo "|> Sucessfully ran [poc_pack_iptables]. Proceeding..."
+
+    if ! poc_pack_qemukjx; then
+        echo "|> Error: could not run [poc_pack_qemukjx]. Exiting now..."
+        return 1
+    fi
+    echo "|> Sucessfully ran [poc_pack_qemukjx]. Proceeding..."
 
 }
 
