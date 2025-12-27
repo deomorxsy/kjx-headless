fac293123dcce159cc3abbed15639f50b0fe0f40
asari <32879431+deomorxsy@users.noreply.github.com>
2025-12-27 11:50:11 -0300

    fix: quick typo from ./poc-aio.sh.sh to ./poc-aio.sh

--- scripts/entrypoints/poc-aio.sh
+++ scripts/entrypoints/poc-aio.sh
@@ -12,7 +12,7 @@ set_qemukjx() {
     # start OCI registry server
     if ! podman start registry; then
         echo "|> WARNING: could not start OCI registry server. Attempting to run the image..."
-        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 01"
+        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 01"
         echo && echo
         #return 1
 
@@ -20,7 +20,7 @@ set_qemukjx() {
 
         if ! [ -f "${ROOTLESS_REGISTRIES:-[EMPTY_VARIABLE]}" ] || (find "${ROOTLESS_REGISTRIES:-[EMPTY_VARIABLE]}" -type f -mtime +7200); then
             echo "|> WARNING: [ROOTLESS_REGISTRIES=${ROOTLESS_REGISTRIES:-[EMPTY_VARIABLE]} filepath does not exist. Attempting to create...]"
-            echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 02"
+            echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 02"
 
             if ! ( (
                 cat <<EOF
@@ -35,7 +35,7 @@ EOF
             echo "|> Successfully redirected the [insecure] configuration for OCI registries at localhost:5000 to [${ROOTLESS_REGISTRIES:-[EMPTY_VARIABLE]}]. Proceeding..."
 
         fi
-        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 02"
+        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 02"
 
         # grep for lines of registries.conf that are not the demonstration of insecure = true being set,
         # i.e. those which do not have a "#" character.
@@ -58,7 +58,7 @@ EOF
         echo && echo
     fi
     echo "|> OCI registry server started with success"
-    echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 01"
+    echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 01"
 
     # check if the image already exists
 
@@ -67,57 +67,57 @@ EOF
         export BUILT_PODMAN_ALREADY
 
         echo "|> WARNING: found a previously built [localhost:5000/qonq_qemukjx]. Attempting to remove image to [REBUILD]..."
-        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 03"
+        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 03"
         echo
 
         if ! (podman rmi "${BUILT_PODMAN_ALREADY:-[EMPTY_VARIABLE]}" --force); then
             echo "|> Error: YOU CAN (NOT) REDO the container image. Literally, it cannot be removed for some reason. No pun intended (or was it?). Exiting now... :)"
-            echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 04"
+            echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 04"
             return 1
         fi
         echo "|> Error: YOU (CAN) REDO the container image. Proceeding..."
-        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 04"
+        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 04"
     fi
     echo "|> WARNING: previously built [localhost:5000/qonq_qemukjx] removed with sucess. ...[PASSED]"
-    echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 03"
+    echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 03"
     #
     if ! (podman images | grep "localhost:5000/qonq_qemukjx"); then
         echo "|> Error: could not find the localhost:5000/qonq_qemukjx image at the OCI registry:3.0 server. Attempting to build now..."
-        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 05"
+        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 05"
         echo && echo
         # return 1
         # Build the podman container with ccr.sh to use  Podman Service as the compose tool
         if ! (CCR_MODE="-checker" . ./scripts/ccr.sh && docker compose -f ./compose.yml --progress=plain build --no-cache qonq_qemukjx); then
             echo "|> Error: could not run the ccr.sh script for Podman Service as the compose tool. Exiting now..."
-            echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 06"
+            echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 06"
             echo && echo
             return 1
         fi
         echo "|> Build the [qonq_qemukjx] container with ccr.sh to use Podman Service as the compose tool with success. Proceeding..."
-        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 06"
+        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 06"
         echo && echo
 
         # push built image into the registry:3.0 localhost:5000 server container.
         if ! (podman push localhost:5000/qonq_qemukjx:latest); then
             echo "|> Error: could not push the built [qonq_qemukjx] image into the OCI registry:3.0 localhost:5000 server container. Exiting now..."
-            echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 07"
+            echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 07"
             return 1
         fi
         echo "|> Pushed built image into the OCI registry:3.0 localhost:5000 server container. Proceeding..."
-        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 07"
+        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 07"
     fi
     echo "|> [qonq_qemukjx] image found at the localhost:5000/qonq_qemukjx OCI registry:3.0 server. Proceeding..."
-    echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 05"
+    echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 05"
 
     # Create the built qonq_qemukjx container
     # podman run -it --name qonq_qemukjx -d localhost:5000/qonq_qemukjx:latest
     if ! (CCR_MODE="-checker" . ./scripts/ccr.sh && docker compose -f ./compose.yml create qonq_qemukjx); then
         echo "|> Error: could not create the built [qonq_qemukjx] container using the ccr.sh script to use Podman Service as the compose tool"
-        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 08"
+        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 08"
         return 1
     fi
     echo "|> Created the built [qonq_qemukjx] container using the ccr.sh script to use Podman Service as the compose tool with success. Proceeding..."
-    echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 08"
+    echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 08"
 
     # check created containers
     CCR_MODE="-checker" . ./scripts/ccr.sh && docker compose ps --all
@@ -129,35 +129,35 @@ EOF
     mkdir -p "${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]}"
     if ! podman cp qonq_qemukjx:/app/qemukjx-tarball-pkg.tar.gz "${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]}"; then
         echo "|> Error: could not copy the podman shared objects tarball to the PACKAGING_ART_DIR=${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]} filepath. Exiting now..."
-        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 09"
+        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 09"
         return 1
     fi
     echo "|> Copied the [qonq_qemukjx] shared objects tarball into the PACKAGING_ART_DIR=${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]} filepath with success. Proceeding... "
-    echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 09"
+    echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 09"
 
     # Stop container registry
     if ! (podman stop registry); then
         echo "|> Error: could not stop the OCI registry server! Exiting now..."
-        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 10"
+        echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 10"
         return 1
     fi
     echo "|> Successfully stopped the OCI registry server."
-    echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh.sh]; check: 10"
+    echo "|> SCOPE: [set_qemukjx], file [./scripts/entrypoints/poc-aio.sh]; check: 10"
 
 }
 
 print_usage() {
     cat <<-END >&2
-USAGE: poc-aio.sh.sh [-options]
+USAGE: poc-aio.sh [-options]
                 - qemukjx
                 - tarball
                 - version
                 - help
 eg,
-MODE="qemukjx"     . ./poc-aio.sh.sh   # create a single tarball of dependencies for qemukjx
-MODE="tarball"  . ./poc-aio.sh.sh   # create a single tarball with all
-MODE="version"  . ./poc-aio.sh.sh   # shows script version
-MODE="help"     . ./poc-aio.sh.sh   # shows this help message
+MODE="qemukjx"     . ./poc-aio.sh   # create a single tarball of dependencies for qemukjx
+MODE="tarball"  . ./poc-aio.sh   # create a single tarball with all
+MODE="version"  . ./poc-aio.sh   # shows script version
+MODE="help"     . ./poc-aio.sh   # shows this help message
 
 See the man page and example file for more info.
 
