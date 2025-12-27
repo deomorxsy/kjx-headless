b1e13f4f402e9d75e4c13fd97eb800aef36aaf03
asari <32879431+deomorxsy@users.noreply.github.com>
2025-12-27 13:56:53 -0300

    fix: add the overall [set-shadow] function setup from the qonq-qdb packaging

--- scripts/packages/usgp-man.sh
+++ scripts/packages/usgp-man.sh
@@ -199,10 +199,143 @@ EOF
 
 set_shadow() {
 
-    CCR_MODE="-checker" . ./scripts/ccr.sh &&
-        docker compose -f ./compose.yml --progress=plain build --no-cache qonq_shadow
+    # CCR_MODE="-checker" . ./scripts/ccr.sh &&
+    #     docker compose -f ./compose.yml --progress=plain build --no-cache qonq_shadow
 
-    echo
+    # echo
+    ROOTLESS_REGISTRIES="./artifacts/rootless-oci/registries.conf"
+    export ROOTLESS_REGISTRIES
+    mkdir -p "$(dirname "${ROOTLESS_REGISTRIES:-[EMPTY_VARIABLE]}")"
+
+    # CCR_MODE="-checker" . ./scripts/ccr.sh &&
+    #     docker compose -f ./compose.yml --progress=plain build --no-cache qonq_shadow
+    #
+
+    # start OCI registry server
+    if ! podman start registry; then
+        echo "|> WARNING: could not start OCI registry server. Attempting to run the image..."
+        echo && echo
+        #return 1
+
+        if ! [ -f "${ROOTLESS_REGISTRIES:-[EMPTY_VARIABLE]}" ]; then
+            echo "|> WARNING: [ROOTLESS_REGISTRIES=${ROOTLESS_REGISTRIES:-[EMPTY_VARIABLE]} filepath does not exist. Attempting to create...]"
+
+            if ! ( (
+                cat <<EOF
+[[registry]]
+location = "localhost:5000"
+insecure = true
+EOF
+            ) | tee "${ROOTLESS_REGISTRIES:-[EMPTY_VARIABLE]}"); then
+                echo "|> Error: could not redirect the [insecure] configuration for OCI registries at localhost:5000 to [${ROOTLESS_REGISTRIES:-[EMPTY_VARIABLE]}]. Exiting now..."
+                return 1
+            fi
+            echo "|> Successfully redirected the [insecure] configuration for OCI registries at localhost:5000 to [${ROOTLESS_REGISTRIES:-[EMPTY_VARIABLE]}]. Proceeding..."
+
+        fi
+
+        # grep for lines of registries.conf that are not the demonstration of insecure = true being set,
+        # i.e. those which do not have a "#" character.
+        if ! (cat /etc/containers/registries.conf | grep "insecure = true" | grep -v "#"); then
+            echo "|> WARNING: it seems there is no [insecure] configuration for registries running locally at localhost:5000. Attempting to use the rootless oci feature of Podman to set the [CONTAINERS_REGISTRIES_CONF] before running podman commands..."
+
+            CONTAINERS_REGISTRIES_CONF="${ROOTLESS_REGISTRIES:-[EMPTY_VARIABLE]}"
+            export CONTAINERS_REGISTRIES_CONF
+
+        fi
+        echo "|> Successfully set the [CONTAINERS_REGISTRIES_CONF] to point to a custom registries.conf [ROOTLESS_REGISTRIES=${ROOTLESS_REGISTRIES:-[EMPTY_VARIABLE]}]. Proceeding..."
+
+        ### # grep for lines of registries.conf that are not the demonstration of insecure = true being set,
+        ### # i.e. those which do not have a "#" character.
+        ### if ! (cat /etc/containers/registries.conf | grep "insecure = true" | grep -v "#"); then
+        ###     echo "|> WARNING: it seems there is no [insecure] configuration for registries running locally at localhost:5000. Attempting to redirect a heredoc to [/etc/containers/registries.conf] to configure..."
+
+        # run the registry:3.0 container image.
+        if ! (podman run -d -p 5000:5000 --name registry registry:3.0); then
+            echo "|> Error: could not run the registry:3.0 container image. Exiting now..."
+            echo && echo
+            return 1
+        fi
+        echo "|> Ran the OCI registry server with success. Proceeding..."
+        echo && echo
+    fi
+    echo "|> OCI registry server started with success"
+
+    # check if the image already exists
+
+    if (podman images | grep "localhost:5000/qonq_shadow"); then
+        BUILT_SHADOW_ALREADY=$(podman images | grep "localhost:5000/qonq_shadow" | awk '{print $3}')
+        export BUILT_SHADOW_ALREADY
+
+        echo "|> WARNING: found a previously built [localhost:5000/qonq_shadow]. Attempting to remove image to [REBUILD]..."
+        echo
+
+        if ! (podman rmi "${BUILT_SHADOW_ALREADY:-[EMPTY_VARIABLE]}" --force); then
+            echo "|> Error: YOU CAN (NOT) REDO the container image. Literally, it cannot be removed for some reason. No pun intended (or was it?). Exiting now... :)"
+            echo
+            return 1
+        fi
+        echo "|> Error: YOU (CAN) REDO the container image. Proceeding..."
+    fi
+    echo "|> WARNING: previously built [localhost:5000/qonq_shadow] removed with sucess. ...[PASSED]"
+    #
+    if ! (podman images | grep "localhost:5000/qonq_shadow"); then
+        echo "|> Error: could not find the localhost:5000/qonq_shadow image at the OCI registry:3.0 server. Attempting to build now..."
+        echo && echo
+        # return 1
+        # Build the shadow container with ccr.sh to use  Podman Service as the compose tool
+        if ! (CCR_MODE="-checker" . ./scripts/ccr.sh && docker compose -f ./compose.yml --progress=plain build --no-cache qonq_shadow); then
+            echo "|> Error: could not run the ccr.sh script for Podman Service as the compose tool. Exiting now..."
+            echo && echo
+            return 1
+
+        fi
+        echo "|> Successfully built the [qonq_shadow] container with ccr.sh to use Podman Service as the compose tool. Proceeding..."
+        echo && echo
+
+        # push built image into the registry:3.0 localhost:5000 server container.
+        if ! (podman push localhost:5000/qonq_shadow:latest); then
+            echo "|> Error: could not push the built [qonq_shadow] image into the OCI registry:3.0 localhost:5000 server container. Exiting now..."
+            echo && echo
+            return 1
+        fi
+        echo "|> Pushed built image into the OCI registry:3.0 localhost:5000 server container. Proceeding..."
+        echo && echo
+    fi
+    echo "|> [qonq_shadow] image found at the localhost:5000/qonq_shadow OCI registry:3.0 server. Proceeding..."
+
+    # Create the built qonq_shadow container
+    # podman run -it --name qonq_shadow -d localhost:5000/qonq_shadow:latest
+    if ! (CCR_MODE="-checker" . ./scripts/ccr.sh && docker compose -f ./compose.yml create qonq_shadow); then
+        echo "|> Error: could not create the built [qonq_shadow] container using the ccr.sh script to use Podman Service as the compose tool"
+        echo && echo
+        return 1
+    fi
+    echo "|> Created the built [qonq_shadow] container using the ccr.sh script to use Podman Service as the compose tool with success. Proceeding..."
+    echo && echo
+
+    # check created containers
+    CCR_MODE="-checker" . ./scripts/ccr.sh && docker compose ps --all
+
+    # check for the qonq_shadow image at localhost:5000/qonq_shadow
+    podman images | grep "localhost:5000/qonq_shadow" | awk '{print $1}'
+
+    # copy qonq_shadow tarball into the ./artifacts/packaging directory.
+    mkdir -p "${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]}"
+    if ! podman cp qonq_shadow:/app/shadow-tarball-pkg.tar.gz "${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]}"; then
+        echo "|> Error: could not copy the full shadow tarball to the PACKAGING_ART_DIR=${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]} filepath. Exiting now..."
+        return 1
+    fi
+    echo "|> Copied the [qonq_shadow] full shadow tarball to the PACKAGING_ART_DIR=${PACKAGING_ART_DIR:-[EMPTY_VARIABLE]} filepath with success. Proceeding... "
+
+    # Stop container registry
+    if ! (podman stop registry); then
+        echo "|> Error: could not stop the OCI registry server! Exiting now..."
+        echo && echo
+        return 1
+    fi
+    echo "|> Successfully stopped the OCI registry server."
+    echo && echo
 }
 
 main_usgp_man() {
